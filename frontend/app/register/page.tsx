02449b80b8310085487d1ba859eaae20cf81b4a1
anudeep2710 <anudeepbatchu10@gmail.com>
2026-01-06 16:28:09 +0530

    fix: improve icon visibility in login/register forms for dark mode

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -60,7 +60,7 @@ export default function Register() {
                                 Full Name
                             </label>
                             <div className="relative">
-                                <User size={16} className="absolute left-3 top-1/2 -translate-y-1/2 text-[var(--text-tertiary)]" />
+                                <User size={16} className="absolute left-3 top-1/2 -translate-y-1/2 text-[var(--text-secondary)] z-10 pointer-events-none" />
                                 <input
                                     id="fullName"
                                     name="fullName"
@@ -79,7 +79,7 @@ export default function Register() {
                                 Email
                             </label>
                             <div className="relative">
-                                <Mail size={16} className="absolute left-3 top-1/2 -translate-y-1/2 text-[var(--text-tertiary)]" />
+                                <Mail size={16} className="absolute left-3 top-1/2 -translate-y-1/2 text-[var(--text-secondary)] z-10 pointer-events-none" />
                                 <input
                                     id="email"
                                     name="email"
@@ -99,7 +99,7 @@ export default function Register() {
                                 Password
                             </label>
                             <div className="relative">
-                                <Lock size={16} className="absolute left-3 top-1/2 -translate-y-1/2 text-[var(--text-tertiary)]" />
+                                <Lock size={16} className="absolute left-3 top-1/2 -translate-y-1/2 text-[var(--text-secondary)] z-10 pointer-events-none" />
                                 <input
                                     id="password"
                                     name="password"
